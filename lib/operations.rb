5640eaef3e33422db68b511690ced19bc7fbbd78
Melissa Ramirez <hdez.melissaa@gmail.com>
2020-02-16 02:36:56 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
+require 'pry'
 def unsafe?(speed)
-
+  if speed > 60
+    true 
+  elsif speed < 40 
+    true 
+  elsif speed >= 40 && speed <= 60 
+    false  
+  end 
 end
 
 
 
 def not_safe?(speed)
-	
+  #(conditional) ? 'true' : 'false'
+  (speed >= 40 && speed <= 60) ? false : true
 end
 	
 
